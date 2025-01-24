673256608146f4d4ff95ed666a55e0079dc2a281
Akpojotor Oghenerurho <108791302+pwoprurho@users.noreply.github.com>
2025-01-24 10:56:38 +0100

    Create Refrence.py
    python script to scan the internet for links related to a query

---
+++ Refrence.py
@@ -0,0 +1,10 @@
+"""
+Author: Akpojotor Oghenerurho
+Email: akporurho@proton.me
+Date:18/01/25
+python script to scan the internet for links related to a keyword
+"""
+from googlesearch import search
+query = input("What do you want to search for")
+for url in search(query):
+  print(url)
